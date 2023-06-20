fc0113cc01c1497715ea232f1a1733fc02f9206b
jiechen <1487503910@qq.com>
2023-06-20 22:05:49 +0800

    feat(factory): add factory pattern

---
+++ src/creational/factory/factory.ts
@@ -0,0 +1,9 @@
+export function bmwFactory(type) {
+	if (type === 'X5') return new Bmw(type, 108000, 300);
+	if (type === 'X6') return new Bmw(type, 111000, 320);
+}
+function Bmw(model, price, maxSpeed) {
+	this.model = model;
+	this.price = price;
+	this.maxSpeed = maxSpeed;
+}
---
+++ test/creational/facatory.spec.ts
@@ -0,0 +1,13 @@
+import { bmwFactory } from '../../src/creational/factory/factory';
+
+describe('factory test', () => {
+	it('sanity', () => {
+		const x5 = bmwFactory('X5');
+		const x6 = bmwFactory('X6');
+
+		expect(x5.price).toBe(108000);
+		expect(x6.price).toBe(111000);
+		expect(x5.maxSpeed).toBe(300);
+		expect(x6.maxSpeed).toBe(320);
+	});
+});
